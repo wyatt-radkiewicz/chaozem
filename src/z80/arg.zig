e4fca6570f2795ca91e77304191435c25197d00f
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2025-10-28 11:54:16 -0600

    Added prefix support for instruction runners in z80 core to allow for iybit and ixbit modes

--- src/z80/arg.zig
+++ src/z80/arg.zig
@@ -8,28 +8,29 @@ const int = @import("int");
 const Cpu = @import("Cpu.zig");
 const exec = @import("exec.zig");
 const Bus = exec.Bus;
+const Prefix = exec.Prefix;
 
 /// 8 bit operand
 pub fn r8(comptime pos: u3) type {
     return struct {
-        pub fn load(cpu: *Cpu, mem: *Bus, _: *Bus, comptime opcode: u8) u8 {
-            return switch (int.extract(u3, opcode, pos)) {
+        pub fn load(cpu: *Cpu, mem: *Bus, _: *Bus, prefix: Prefix) u8 {
+            return switch (int.extract(u3, prefix.bytes, pos)) {
                 0...5 => |n| @as(Cpu.Bank.R8, @enumFromInt(n)).get(cpu.banks[cpu.set]),
                 6 => exec.read(mem, u8, Cpu.Bank.R16.hl.get(cpu.banks[cpu.set])),
                 7 => cpu.banks[cpu.set].a,
             };
         }
 
-        pub fn store(cpu: *Cpu, mem: *Bus, _: *Bus, comptime opcode: u8, value: u8) void {
-            switch (int.extract(u3, opcode, pos)) {
+        pub fn store(cpu: *Cpu, mem: *Bus, _: *Bus, prefix: Prefix, value: u8) void {
+            switch (int.extract(u3, prefix.bytes, pos)) {
                 0...5 => |n| @as(Cpu.Bank.R8, @enumFromInt(n)).set(cpu.banks[cpu.set], value),
                 6 => exec.write(mem, u8, Cpu.Bank.R16.hl.get(cpu.banks[cpu.set]), value),
                 7 => cpu.banks[cpu.set].a = value,
             }
         }
 
-        pub fn disasm(writer: *Writer, _: *Reader, opcode: u8) WriteError!void {
-            try writer.print("{s}", .{switch (int.extract(u3, opcode, pos)) {
+        pub fn disasm(writer: *Writer, _: *Reader, opcode: Prefix) WriteError!void {
+            try writer.print("{s}", .{switch (int.extract(u3, opcode.bytes, pos)) {
                 0...5 => |n| @tagName(@as(Cpu.Bank.R8, @enumFromInt(n))),
                 6 => "[hl]",
                 7 => "a",
@@ -41,22 +42,22 @@ pub fn r8(comptime pos: u3) type {
 /// 16 bit operand
 pub fn r16(comptime pos: u3) type {
     return struct {
-        pub fn load(cpu: *Cpu, _: *Bus, _: *Bus, comptime opcode: u8) u16 {
-            return switch (int.extract(u2, opcode, pos)) {
+        pub fn load(cpu: *Cpu, _: *Bus, _: *Bus, prefix: Prefix) u16 {
+            return switch (int.extract(u2, prefix.bytes, pos)) {
                 0...2 => |n| @as(Cpu.Bank.R16, @enumFromInt(n)).get(cpu.banks[cpu.set]),
                 3 => cpu.sp,
             };
         }
 
-        pub fn store(cpu: *Cpu, _: *Bus, _: *Bus, comptime opcode: u8, value: u16) void {
-            switch (int.extract(u2, opcode, pos)) {
+        pub fn store(cpu: *Cpu, _: *Bus, _: *Bus, prefix: Prefix, value: u16) void {
+            switch (int.extract(u2, prefix.bytes, pos)) {
                 0...2 => |n| @as(Cpu.Bank.R16, @enumFromInt(n)).set(cpu.banks[cpu.set], value),
                 3 => cpu.sp = value,
             }
         }
 
-        pub fn disasm(writer: *Writer, _: *Reader, opcode: u8) WriteError!void {
-            try writer.print("{s}", .{switch (int.extract(u2, opcode, pos)) {
+        pub fn disasm(writer: *Writer, _: *Reader, prefix: Prefix) WriteError!void {
+            try writer.print("{s}", .{switch (int.extract(u2, prefix.bytes, pos)) {
                 0...2 => |n| @tagName(@as(Cpu.Bank.R16, @enumFromInt(n))),
                 3 => "sp",
             }});
@@ -67,11 +68,11 @@ pub fn r16(comptime pos: u3) type {
 /// Immediate operand
 pub fn imm(comptime Int: type) type {
     return struct {
-        pub fn load(cpu: *Cpu, mem: *Bus, _: *Bus, comptime _: u8) Int {
+        pub fn load(cpu: *Cpu, mem: *Bus, _: *Bus, _: Prefix) Int {
             return exec.fetch(cpu, mem, Int);
         }
 
-        pub fn disasm(writer: *Writer, reader: *Reader, _: u8) WriteError!void {
+        pub fn disasm(writer: *Writer, reader: *Reader, _: Prefix) WriteError!void {
             const data = reader.takeInt(Int, .little) orelse return error.WriteFailed;
             switch (@bitSizeOf(Int)) {
                 8 => try writer.print("#${x:0>2}", .{data}),
--- src/z80/exec.zig
+++ src/z80/exec.zig
@@ -8,20 +8,41 @@ const Cpu = @import("Cpu.zig");
 pub const bus_width = bus_interface.Width{ .addr = 16, .data = 8 };
 pub const Bus = bus_interface.Bus(bus_width);
 
+/// Used when creating prefixes for instructions
+pub const Prefix = packed struct {
+    /// The bytes that make up the instruction
+    bytes: Int = 0,
+
+    /// Gets opcode byte n
+    pub fn opcode(this: @This(), n: std.math.IntFittingRange(0, max_len - 1)) u8 {
+        return @truncate(this.bytes >> @as(std.math.Log2Int(Int), n * 8));
+    }
+
+    /// Adds a new byte to the start of the prefix
+    pub fn push(this: *@This(), byte: u8) void {
+        this.bytes <<= 8;
+        this.bytes |= byte;
+    }
+
+    /// Maximum length of a instruction prefix
+    pub const max_len = 8;
+    pub const Int = std.meta.Int(.unsigned, max_len * 8);
+};
+
 /// Generate a 256 entry decoder given a list of decoders, solves collisions with priority
-pub fn decoder(comptime decoders: []const Decoder) Fetch {
+pub fn decoder(comptime decoders: []const Pattern) Fetch.Decoder {
     // Sort the decoders by priority (lowest priority first)
     comptime var sorted = decoders.*;
-    comptime std.sort.pdq(Decoder, &sorted, void, struct {
-        pub fn lessThan(_: void, lhs: Decoder, rhs: Decoder) bool {
+    comptime std.sort.pdq(Pattern, &sorted, void, struct {
+        pub fn lessThan(_: void, lhs: Pattern, rhs: Pattern) bool {
             return rhs.priority < lhs.priority;
         }
     }.lessThan);
 
     // Add them into a look up table
-    comptime var lut = [1]Fetch{struct {
-        pub fn inner(_: *Cpu, _: *Bus) ?Instr {
-            return null;
+    comptime var lut = [1]Fetch.Decoder{struct {
+        pub fn inner(_: *Cpu, _: *Bus) Fetch {
+            return .{};
         }
     }.inner} ** 0x100;
     inline for (sorted) |decode| {
@@ -31,8 +52,11 @@ pub fn decoder(comptime decoders: []const Decoder) Fetch {
     // Return a function that looks up the look up table
     const final_lut = lut;
     return struct {
-        pub fn inner(cpu: *Cpu, bus: *Bus) ?Instr {
-            return final_lut[fetch(cpu, bus, u8)](cpu, bus);
+        pub fn inner(cpu: *Cpu, bus: *Bus) Fetch {
+            const opcode = fetch(cpu, bus, u8);
+            var next = final_lut[opcode](cpu, bus);
+            next.prefix.push(opcode);
+            return next;
         }
     }.inner;
 }
@@ -45,7 +69,7 @@ pub fn instr(
     comptime op: anytype,
     comptime dst: ?type,
     comptime src: ?type,
-) []const Decoder {
+) []const Pattern {
     // Generate pattern matching int's to generate the patterns
     comptime var set: u8 = 0;
     comptime var any: u8 = 0;
@@ -61,8 +85,8 @@ pub fn instr(
     }
 
     // Generate each pattern
-    var buffer: [std.mem.count(u8, pattern, "x")]Decoder = undefined;
-    var decoders = std.ArrayList(Decoder).initBuffer(&buffer);
+    var buffer: [std.mem.count(u8, pattern, "x")]Pattern = undefined;
+    var decoders = std.ArrayList(Pattern).initBuffer(&buffer);
     for (0..0x100) |byte| {
         if (byte & ~any ^ set != 0) {
             continue;
@@ -71,8 +95,8 @@ pub fn instr(
             .priority = any,
             .opcode = byte,
             .fetch = struct {
-                pub fn inner(_: *Cpu, _: *Bus) ?Instr {
-                    return Instr.init(byte, name, clk, op, dst, src);
+                pub fn inner(_: *Cpu, _: *Bus) Fetch {
+                    return .{ .instr = Instr.init(byte, name, clk, op, dst, src) };
                 }
             }.inner,
         });
@@ -84,29 +108,39 @@ pub fn instr(
 }
 
 /// Represents the data needed to decode an instruction and the function to fetch it
-pub const Decoder = struct {
+pub const Pattern = struct {
     /// Priority of this pattern in a decoding lut, 0 is highest priority
-    priority: u8,
+    priority: u8 = std.math.maxInt(u8),
 
     /// What opcode this pattern is for
     opcode: u8,
 
     /// Fetch and get the instruction
-    fetch: Fetch,
+    fetch: Fetch.Decoder,
 };
 
-/// The actual function that will fetch an instruction
-pub const Fetch = *const fn (cpu: *Cpu, mem: *Bus) ?Instr;
+/// Result of decoding an instruction
+pub const Fetch = struct {
+    /// Prefix bytes
+    prefix: Prefix = .{},
+
+    /// Instruction decoded
+    instr: ?Instr = null,
+
+    /// The actual function that will fetch an instruction
+    pub const Decoder = *const fn (cpu: *Cpu, mem: *Bus) @This();
+};
 
 /// Result of fetching and decoding an instruction
 pub const Instr = struct {
     /// Run the instruction with the cpu, memory bus, and io bus and returns the # of cycles
-    run: *const fn (cpu: *Cpu, mem: *Bus, io: *Bus) usize,
+    run: *const fn (cpu: *Cpu, mem: *Bus, io: *Bus, prefix: Prefix) usize,
 
     /// Disassembles the instruction using the bytes at reader
     disasm: *const fn (
         writer: *std.io.Writer,
         reader: *std.io.Reader,
+        prefix: Prefix,
     ) std.io.Writer.Error!void,
 
     /// Create a specific instruction
@@ -116,7 +150,6 @@ pub const Instr = struct {
     /// arguments. 'dst' may be ommitted but can still be written to. If both arguments are in the
     /// op handler, then both src and dest have to be supplied.
     pub fn init(
-        comptime opcode: u8,
         comptime name: []const u8,
         comptime clk: usize,
         comptime op: anytype,
@@ -125,39 +158,39 @@ pub const Instr = struct {
     ) Instr {
         return .{
             .run = struct {
-                pub fn run(cpu: *Cpu, mem: *Bus, io: *Bus) usize {
+                pub fn run(cpu: *Cpu, mem: *Bus, io: *Bus, prefix: Prefix) usize {
                     switch (@as(u2, @intFromBool(arg_dst != null)) << 1 |
                         @as(u2, @intFromBool(arg_src != null))) {
                         0b00 => _ = op(cpu),
                         0b01 => {
                             const src = arg_src orelse unreachable;
-                            const src_data = src.load(cpu, mem, io, opcode);
+                            const src_data = src.load(cpu, mem, io, prefix);
                             _ = op(cpu, src_data);
                         },
                         0b10 => {
                             const dst = arg_dst orelse unreachable;
-                            const dst_data = dst.load(cpu, mem, io, opcode);
+                            const dst_data = dst.load(cpu, mem, io, prefix);
                             const res = switch (@typeInfo(@TypeOf(op)).@"fn".params.len) {
                                 2 => op(cpu, dst_data),
                                 1 => op(cpu),
                                 else => @compileError("Expected 1 or 2 arguments!"),
                             };
                             if (@hasDecl(dst, "store")) {
-                                dst.store(cpu, mem, io, res);
+                                dst.store(cpu, mem, io, prefix, res);
                             }
                         },
                         0b11 => {
                             const dst = arg_dst orelse unreachable;
                             const src = arg_src orelse unreachable;
-                            const dst_data = dst.load(cpu, mem, io, opcode);
-                            const src_data = src.load(cpu, mem, io, opcode);
+                            const dst_data = dst.load(cpu, mem, io, prefix);
+                            const src_data = src.load(cpu, mem, io, prefix);
                             const res = switch (@typeInfo(@TypeOf(op)).@"fn".params.len) {
                                 3 => op(cpu, dst_data, src_data),
                                 2 => op(cpu, src_data),
                                 else => @compileError("Expected 2 or 3 arguments!"),
                             };
                             if (@TypeOf(res) != void and @hasDecl(dst, "store")) {
-                                dst.store(cpu, mem, io, opcode, res);
+                                dst.store(cpu, mem, io, prefix, res);
                             }
                         },
                     }
@@ -168,17 +201,18 @@ pub const Instr = struct {
                 pub fn disasm(
                     writer: *std.io.Writer,
                     reader: *std.io.Reader,
+                    prefix: Prefix,
                 ) std.io.Writer.Error!void {
                     try writer.print("{s}", .{name});
                     const has_dst = @hasDecl(arg_dst orelse struct {}, "disasm");
                     const has_src = @hasDecl(arg_src orelse struct {}, "disasm");
                     if (has_dst) {
                         try writer.print(" ", .{});
-                        try (arg_dst orelse unreachable).disasm(writer, reader, opcode);
+                        try (arg_dst orelse unreachable).disasm(writer, reader, prefix);
                     }
                     if (has_src) {
                         try writer.print("{s}", .{if (has_dst) "," else " "});
-                        try (arg_src orelse unreachable).disasm(writer, reader, opcode);
+                        try (arg_src orelse unreachable).disasm(writer, reader, prefix);
                     }
                 }
             },
